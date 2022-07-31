464eefda0ea4ca584fe18978734c24649994b6e1
Иван Морозов <vanodeshka@gmail.com>
2022-07-31 18:03:30 +0300

    models_serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
 
-from recipes.models import Tag, Ingredients, Recipe
+from recipes.models import Tag, Ingredients, Recipe, User, Follow
+from rest_framework.validators import UniqueTogetherValidator
 
 
-class TagSerializer(serializers.Serializer):
+class TagSerializer(serializers.ModelSerializer):
     slug = serializers.SlugRelatedField(
         slug_field='slug',
         many=True
@@ -14,16 +15,16 @@ class TagSerializer(serializers.Serializer):
         fields = ('title', 'color')
 
 
-class IngredientsSerializer(serializers.Serializer):
+class IngredientsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredients
-        fields = '__all__'
+        fields = ('__all__',)
 
 
-class RecipeSerializer(serializers.Serializer):
+class RecipeSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
         read_only=True,
-        slug_field='author'
+        slug_field='username'
     )
     ingredients = serializers.SlugRelatedField(
         read_only=True,
@@ -37,3 +38,35 @@ class RecipeSerializer(serializers.Serializer):
     class Meta:
         model = Recipe
         fields = ('__all__',)
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username',
+        default=serializers.CurrentUserDefault()
+    )
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        required=True,
+        queryset=User.objects.all()
+    )
+
+    class Meta:
+        fields = ('user', 'author')
+        model = Follow
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following')
+            )
+        ]
+
+
+class FavouriteSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username'
+    )
+    recipe = serializers.SlugRelatedField(
+        slug_field='recipe'
+    )
